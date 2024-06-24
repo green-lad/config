afd2f82460cf45faffdf8c499d37c8d3726f2c8e
Markus Schoetz <markus.schoetz@fau.de>
2024-06-24 16:29:37 +0200

    Change link creation

--- create_links.sh
+++ create_links.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
+links=(
+  "$XDG_CONFIG_HOME/zsh/zshrc,~/.zshrc"
+  "$XDG_CONFIG_HOME/xorg/xinitrc,~/.xinitrc"
+  "$XDG_CONFIG_HOME/xorg/xbindkeysrc,~/.xbindkeysrc"
+  "$XDG_CONFIG_HOME/firefox,~/.mozilla/firefox"
+  "$XDG_CONFIG_HOME/moc,~/.moc"
+  "$XDG_CONFIG_HOME/gtk-3.0/themes,~/.themes"
+)
 
-SCRIPTPATH=$(dirname "$0")
-
-while IFS="," read -r target name remaining_columns
+IFS=$','
+for i in "${links[@]}"
 do
-  eval target="$target"
-  eval name="$name"
+  set -- $i # Convert "tuple" into the param args $1 $2...
+  eval target="$1"
+  eval name="$2"
   ln -sf $target $name
-done < <(tail -n +2 "$SCRIPTPATH/links.csv")
+done
+
