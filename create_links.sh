433220f43bf2b5f23e58f3ae5d9e709915a38569
Markus Schoetz <markus.schoetz@fau.de>
2024-06-24 16:05:33 +0100

    Simplify link creation to work with /bin/sh

--- create_links.sh
+++ create_links.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-links=(
-  "$XDG_CONFIG_HOME/zsh/zshrc,~/.zshrc"
-  "$XDG_CONFIG_HOME/xorg/xinitrc,~/.xinitrc"
-  "$XDG_CONFIG_HOME/xorg/xbindkeysrc,~/.xbindkeysrc"
-  "$XDG_CONFIG_HOME/firefox,~/.mozilla/firefox"
-  "$XDG_CONFIG_HOME/moc,~/.moc"
+set \
+  "$XDG_CONFIG_HOME/zsh/zshrc,~/.zshrc" \
+  "$XDG_CONFIG_HOME/xorg/xinitrc,~/.xinitrc" \
+  "$XDG_CONFIG_HOME/xorg/xbindkeysrc,~/.xbindkeysrc" \
+  "$XDG_CONFIG_HOME/firefox,~/.mozilla/firefox" \
+  "$XDG_CONFIG_HOME/moc,~/.moc" \
   "$XDG_CONFIG_HOME/gtk-3.0/themes,~/.themes"
-)
 
 IFS=$','
-for i in "${links[@]}"
+for i
 do
   set -- $i # Convert "tuple" into the param args $1 $2...
   eval target="$1"
-  eval name="$2"
-  ln -sf $target $name
+  eval path="$2"
+  dir=${path%/*}
+  (test -d "$dir" || mkdir "$dir") && ln -sf "$target" "$path"
 done
 
