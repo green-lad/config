23a64310b5f00ceb0b80d382268bcf03af2b5e01
Markus Schoetz <markus.schoetz@fau.de>
2025-03-12 11:21:56 +0100

    Use nftables firewall

--- system/configuration.nix
+++ system/configuration.nix
@@ -199,12 +199,53 @@
     # needed for zfs
     hostId = "8425e349";
     wireless.iwd.enable = true;
-    firewall = {
-      enable = true;
-      allowedTCPPorts = [ 443 80 ];
-      allowedUDPPorts = [ 443 80 44857 ];
-      allowPing = false;
-    };
+    nftables.ruleset = ''
+      # Check out https://wiki.nftables.org/ for better documentation.
+      # Table for both IPv4 and IPv6.
+      table inet filter {
+        # Block all incoming connections traffic except SSH and "ping".
+        chain input {
+          type filter hook input priority 0;
+      
+          # accept any localhost traffic
+          iifname lo accept
+      
+          # accept traffic originated from us
+          ct state {established, related} accept
+      
+          # ICMP
+          # routers may also want: mld-listener-query, nd-router-solicit
+          ip6 nexthdr icmpv6 icmpv6 type { destination-unreachable, packet-too-big, time-exceeded, parameter-problem, nd-router-advert, nd-neighbor-solicit, nd-neighbor-advert } accept
+          ip protocol icmp icmp type { destination-unreachable, router-advertisement, time-exceeded, parameter-problem } accept
+      
+          # allow "ping"
+          ip6 nexthdr icmpv6 icmpv6 type echo-request accept
+          ip protocol icmp icmp type echo-request accept
+      
+          tcp dport {ssh,http,https} accept
+      
+          # count and drop any other traffic
+          counter drop
+        }
+      
+        # Allow all outgoing connections.
+        chain output {
+          type filter hook output priority 0;
+          accept
+        }
+      
+        chain forward {
+          type filter hook forward priority 0;
+          accept
+        }
+      }
+    '';
+    # firewall = {
+    #   enable = true;
+    #   allowedTCPPorts = [ 443 80 ];
+    #   allowedUDPPorts = [ 443 80 44857 ];
+    #   allowPing = false;
+    # };
     interfaces.enp0s25 = {
       ipv4.addresses = [{
         address = "10.10.10.1";
