a8b2bb1b05b4120c0ca5cad330117d8f97199822
Markus Schoetz <markus.schoetz@fau.de>
2025-03-26 15:17:09 +0100

    use host hardware configuration

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,7 +1,6 @@
 { config, pkgs, inputs, user, hostname, ... }: {
   imports = [
-    # ../host_hardware/${hostname}
-    ../host_hardware/x230/default.nix
+    ../host_hardware/${hostname}
   ];
   
   environment = {
