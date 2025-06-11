e95e9445ed1dff5e8753ecfec09d78c56215a126
Markus Schoetz <markus.schoetz@fau.de>
2025-06-11 14:31:52 +0200

    add FAU.fm wifi

--- nixos/network_profiles.nix
+++ nixos/network_profiles.nix
@@ -4,6 +4,37 @@ environmentFile:
 {
   environmentFiles = [ environmentFile ];
   profiles = {
+    fau_fm = {
+      connection = {
+        id = "fau";
+        type = "wifi";
+        permissions = "user:markus:;";
+      };
+      ipv4 = {
+        method = "auto";
+      };
+      ipv6 = {
+        addr-gen-mode = "default";
+        method = "auto";
+      };
+      wifi = {
+        ssid = "FAU.fm";
+      };
+      wifi-security = {
+        group = "ccmp;tkip;";
+        key-mgmt= "wpa-eap";
+        pairwise = "ccmp;";
+        proto = "rsn;";
+      };
+      "802-1x" = {
+        anonymous-identity = "$fau_user";
+        ca-cert = "/home/markus/.config/cat_installer/comodo.pem";
+        eap = "peap;";
+        identity = "$fau_user";
+        password = "$fau_password";
+        phase2-auth = "mschapv2";
+      };
+    };
     home_wlan = {
       connection = {
         id = "home_wlan";
